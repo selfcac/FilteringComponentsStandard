bb25d32edd55989cb1bb3c41efa018702f36c069
selfcac <selfcac@5gbinbox.com>
2020-01-03 00:17:11 +0200

    Change project name to avoid problems in assembly imports

--- CheckBlacklistedWifi/GitInfo.cs
+++ CheckBlacklistedWifi/GitInfo.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace CheckBlacklistedWifi
+namespace CheckBlacklistedWifiStandard
 {
     public static class GitInfo
     {
--- CheckBlacklistedWifi/Properties/Resources.Designer.cs
+++ CheckBlacklistedWifi/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace CheckBlacklistedWifi.Properties {
+namespace CheckBlacklistedWifiStandard.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace CheckBlacklistedWifi.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("CheckBlacklistedWifi.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("CheckBlacklistedWifiStandard.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
