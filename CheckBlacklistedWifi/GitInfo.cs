6147ec7f54d7e29b03c6e231670f9262f4328565
selfcac <selfcac@5gbinbox.com>
2020-01-02 22:54:08 +0200

    Git info for each project

---
+++ CheckBlacklistedWifi/GitInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CheckBlacklistedWifi
+{
+    public static class GitInfo
+    {
+        public static string GetInfo()
+        {
+            return Properties.Resources.GitInfo;
+        }
+    }
+}
---
+++ HTTPProtocolFilter/GitInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HTTPProtocolFilter
+{
+    public static class GitInfo
+    {
+        public static string GetInfo()
+        {
+            return Properties.Resources.GitInfo;
+        }
+    }
+}
