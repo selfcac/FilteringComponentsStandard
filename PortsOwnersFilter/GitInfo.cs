556df3d1e6e8b24c3f1fd5ef00a13cd0a15225b8
selfcac <selfcac@5gbinbox.com>
2020-03-24 16:46:43 +0200

    Starting PortsOwnersFilter

---
+++ PortsOwnersFilter/GitInfo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PortsOwnersFilter
+{
+    public class GitInfo
+    {
+        static string[] GitDependencies =
+            CommonStandard.GitInfo.AllGitInfo()
+        ;
+
+        public static string GetInfo()
+        {
+            return string.Format("{0} {1}+{2}{3} ({4}) {5}",
+                typeof(GitInfo).Namespace,
+                ThisAssembly.Git.BaseTag,
+                ThisAssembly.Git.Commits,
+                ThisAssembly.Git.IsDirty ? "*" : "",
+                ThisAssembly.Git.Branch,
+                ThisAssembly.Git.Commit
+                );
+        }
+
+        public static string[] AllGitInfo()
+        {
+            List<string> dependencies = GitDependencies.Select(x => "+-- " + x).ToList();
+            dependencies.Insert(0, GetInfo());
+            return dependencies.ToArray();
+        }
+    }
+}
---
+++ PortsOwnersFilter/IPortsOwnerPolicy.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PortsOwnersFilter
+{
+    public enum OwnerType
+    {
+        NAME= 0,
+        SID,
+    }
+
+    public enum PathAllowedType
+    {
+        GroupOrUsers,
+        ANY
+    }
+
+    public enum FilterMode
+    {
+        // Only allow bypass if match the policy
+        Whitelist,
+
+        // Only force filter if match the policy
+        //      trusted pid are still always bypass
+        Blacklist,
+    }
+
+    // S-1-5-32-544	Admin
+
+    public class UserOwner
+    {
+        OwnerType Type;
+        string Value;
+    }
+
+    public class GroupOwner
+    {
+        OwnerType Type;
+        string Value;
+    }
+
+    public class PathPolicy
+    {
+        string Path;
+        PathAllowedType PathType;
+
+        UserOwner[] AllowedUsers;
+        GroupOwner[] AllowedGroups;
+    }
+
+    // you get pid 
+    //  -> you get {Username, ProcessPath} 
+    //  -> you get {Username, [User.Groups], ProcessPath}
+    public interface IPortsOwnerPolicy
+    {
+        FilterMode getMode();
+        void setMode(FilterMode mode);
+
+        // You can remember trusted process ids (like the host process of this filter etc.)
+        void addTrustedPid(int pid);
+        void isTrustedPid(int pid);
+
+        // You can ignore Path if user is trusted
+        bool isUserTrusted(UserOwner user);
+
+        // You can ignore Path if user in group that is trusted
+        bool isGroupsTrusted(GroupOwner[] groups);
+
+        // Finally you check if path is allowed (with group, user, any user tuple)
+        bool isProcessPathAllowed(string fullpath, UserOwner user, GroupOwner[] groups);
+
+        // Check all (pid, trusted user, trusted group, path with those user\groups)
+        bool isAllowed(int pid, string fullpath, UserOwner user, GroupOwner[] groups);
+    }
+}
